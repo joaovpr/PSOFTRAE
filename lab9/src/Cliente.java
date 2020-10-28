6a520b42c28f4c78f8b81b2a4993fd4df1481d7e
João Vitor Patricio Romão <50140733+joaovpr@users.noreply.github.com>
2020-10-28 17:20:48 -0300

    Update Cliente.java

--- lab9/src/Cliente.java
+++ lab9/src/Cliente.java
@@ -1,9 +1,7 @@
-<<<<<<< HEAD
+
 //IMPLEMENTAÇÃO DO LAB 9 PSOFT
 //JOÃO VITOR PATRÍCIO ROMÃO - MATRÍCULA: 118211058
-=======
 
->>>>>>> 02b12ba07291f2d4dedd144d43694982e26826af
 
 
 //PROBLEMAS NO PROJETO ORIGINAL:
@@ -17,17 +15,13 @@
 
 public class Cliente {
 	public static void main(String[] args) {
-<<<<<<< HEAD
+
 	    FabricaBolo fBoloBaunilha = new Baunilha();
 		FabricaBolo fBoloChocolate = new Chocolate();
 		//set do sabor da cobertura para teste
 		fBoloBaunilha.setMorango();
 		fBoloChocolate.setCaramelo();
 		//fabricação dos bolos com massa e cobertura desejadas
-=======
-		FabricaBolo fBoloBaunilha = new BaunilhaComMorango();
-		FabricaBolo fBoloChocolate = new ChocolateComCaramelo();
->>>>>>> 02b12ba07291f2d4dedd144d43694982e26826af
 		fBoloBaunilha.prepararBolo();
 		fBoloChocolate.prepararBolo();
 	}
